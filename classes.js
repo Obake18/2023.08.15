e725f6a8a698c41e9855e40fe0e3af98a3471e9c
Obake18 <miltondesenhista@gmail.com>
2023-08-29 13:40:07 -0300

    aGORA VAI

---
+++ classes.js
@@ -0,0 +1,9 @@
+class Musica{
+    constructor(capa, titulo, artista, album){
+        
+        this.capa = capa;
+        this.titulo = titulo;
+        this.album = album;
+        this.artista = artista;   
+    }    
+}
